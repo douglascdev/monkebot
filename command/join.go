c1692f43fa4a475b7a2c872bf1551d337bfca95f
douglascdev <douglasc.dev@gmail.com>
2024-09-19 11:00:20 -0300

    Check if channel was already joined before trying to join

--- command/join.go
+++ command/join.go
@@ -74,16 +74,59 @@ var join = Command{
 			}{ID: message.Chatter.ID, Name: message.Chatter.Name})
 		}
 
+		// check if any of the channels are already in the database
+		var (
+			query    string
+			rows     *sql.Rows
+			channels []interface{}
+		)
+		query = fmt.Sprintf("SELECT name FROM user WHERE name IN (%s) AND bot_is_joined", strings.Repeat("?,", max(0, len(channelsToJoin)-1))+"?")
+		channels = make([]interface{}, len(channelsToJoin))
+		for i, channel := range channelsToJoin {
+			channels[i] = channel.Name
+		}
+
+		rows, err = tx.Query(query, channels...)
+		if err == nil {
+			defer rows.Close()
+			var foundChannels []string
+			for rows.Next() {
+				var name string
+				err = rows.Scan(&name)
+				if err != nil {
+					return err
+				}
+				foundChannels = append(foundChannels, name)
+			}
+			err = rows.Err()
+			if err != nil {
+				return err
+			}
+
+			if len(foundChannels) > 0 {
+				answer := fmt.Sprintf("❌The following channels were already joined: %s", strings.Join(foundChannels, ", "))
+				sender.Say(message.Channel, answer)
+				return nil
+			}
+		}
+
 		err = database.InsertUsers(tx, true, channelsToJoin...)
 		if err != nil {
 			sender.Say(message.Channel, "❌Command failed, please try again or contact an admin")
 			return err
 		}
 
-		var (
-			commandNames []string
-			rows         *sql.Rows
-		)
+		// ensure all joined channels have bot_is_joined set to true if InsertUsers didn't just insert them(it skips existing users)
+		var channelIDs []string
+		for _, channel := range channelsToJoin {
+			channelIDs = append(channelIDs, channel.ID)
+		}
+		err = database.UpdateIsBotJoined(tx, true, channelIDs...)
+		if err != nil {
+			return err
+		}
+
+		var commandNames []string
 		rows, err = tx.Query("SELECT name FROM command")
 		if err != nil {
 			return err
