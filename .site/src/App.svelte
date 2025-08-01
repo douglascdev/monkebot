c1de4bf8ee6283ddad4bcfc59cf9c81fb50a2ce2
douglascdev <douglasc.dev@gmail.com>
2025-08-01 13:35:47 -0300

    Show bool values as checkboxes

--- .site/src/App.svelte
+++ .site/src/App.svelte
@@ -16,8 +16,12 @@
       <td>${command.Description}</td>
       <td>${command.ChannelCooldown}</td>
       <td>${command.UserCooldown}</td>
-      <td>${command.NoPrefix}</td>
-      <td>${command.CanDisable}</td>
+      <td>
+        <input type="checkbox" disabled ${command.NoPrefix ? "checked=checked" : ""} class="checkbox" />
+      </td>
+      <td>
+        <input type="checkbox" disabled ${command.CanDisable ? "checked=checked" : ""} class="checkbox" />
+      </td>
     `;
 
     tableBody.appendChild(row);
@@ -62,19 +66,5 @@ fetch(url)
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+
 </style>
