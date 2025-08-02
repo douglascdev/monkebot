a5c5f00a5578a1066ff8e893c15d9248af8b4d4f
douglascdev <douglasc.dev@gmail.com>
2025-08-02 13:24:16 -0300

    Load commands

--- .site/src/routes/+page.svelte
+++ .site/src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import { onMount } from 'svelte';
+  let commands = [];
+
+  onMount(async () => {
+    
+        const res = await fetch(`${import.meta.env.BASE_URL}commands.json`);
+    commands = await res.json();
+  });
+</script>
+
+<table>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Usage</th>
+      <th>Description</th>
+      <th>Cooldowns</th>
+    </tr>
+  </thead>
+  <tbody>
+    {#each commands as cmd}
+      <tr>
+        <td>{cmd.Name}</td>
+        <td>{cmd.Usage}</td>
+        <td>{cmd.Description}</td>
+        <td>{cmd.ChannelCooldown}</td>
+        <td>{cmd.UserCooldown}</td>
+      </tr>
+    {/each}
+  </tbody>
+</table>
+
