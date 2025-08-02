d6012b5f5a04254b00373781de11bfcf1ce33372
douglascdev <douglasc.dev@gmail.com>
2025-08-02 13:39:11 -0300

    Add navbar

--- .site/src/routes/+page.svelte
+++ .site/src/routes/+page.svelte
@@ -9,6 +9,15 @@
   });
 </script>
 
+<div class="navbar bg-base-100 shadow-sm">
+  <div class="flex-1">
+    <a class="btn btn-ghost text-xl">monkebot</a>
+  </div>
+  <div class="flex gap-2">
+    <input type="text" placeholder="Search" disabled class="input input-bordered w-24 md:w-auto" />
+  </div>
+</div>
+
 <div class="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
 <table class="table">
   <thead>
