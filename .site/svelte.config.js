14bd6000fd1daa5a465e5b3f4395c354304985fa
douglascdev <douglasc.dev@gmail.com>
2025-08-02 13:11:37 -0300

    Add daisyUI

--- .site/svelte.config.js
+++ .site/svelte.config.js
@@ -6,7 +6,15 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	
+	kit: {
+	  adapter: adapter({
+	    fallback: '404.html',
+	  }),
+	  paths: {
+	    base: '/monkebot'
+	  }
+	}
 };
 
 export default config;
